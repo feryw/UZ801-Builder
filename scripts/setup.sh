c3859c18a36decbd906d7756c60934aea3b1c217
wyref <ferywu@gmail.com>
2025-09-23 16:44:07 +0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,9 @@
 DEBIAN_FRONTEND=noninteractive
 DEBCONF_NONINTERACTIVE_SEEN=true
 
-echo 'tzdata tzdata/Areas select Etc' | debconf-set-selections
-echo 'tzdata tzdata/Zones/Etc select UTC' | debconf-set-selections
+echo 'tzdata tzdata/Areas select Asia' | debconf-set-selections
+echo 'tzdata tzdata/Zones/Asia select Jakarta' | debconf-set-selections
+dpkg-reconfigure -f noninteractive tzdata
 echo "locales locales/default_environment_locale select en_US.UTF-8" | debconf-set-selections
 echo "locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8" | debconf-set-selections
 rm -f "/etc/locale.gen"
@@ -17,15 +18,18 @@ apt install -qqy --no-install-recommends \
     dnsmasq \
     hostapd \
     iptables \
-    nftables \
+    libconfig9 \
     htop \
     nano \
+    neofetch \
+    iftop \
     locales \
     modemmanager \
     netcat-traditional \
     net-tools \
     network-manager \
     openssh-server \
+    qrtr-tools \
     rmtfs \
     sudo \
     systemd-timesyncd \

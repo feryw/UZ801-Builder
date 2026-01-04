381d4ee6e1480b22aa388e790b9e0732fd9214e3
wyref <ferywu@gmail.com>
2026-01-04 23:20:58 +0700

    Update build_hyp_aboot.sh

--- scripts/build_hyp_aboot.sh
+++ scripts/build_hyp_aboot.sh
@@ -1,12 +1,30 @@
 #!/bin/sh -e
 
+TYPEDEV="${DEV_TYPE:?DEV_TYPE not set}"
+
+case "$TYPEDEV" in
+  uz801)
+    DESCR="yiming,uz801-v3"
+    ;;
+  uf896)
+    DESCR="thwc,uf896"
+    ;;
+  ufi001)
+    DESCR="thwc,ufi001c"
+    ;;  
+  *)
+    echo "Unsupported device: $TYPEDEV" >&2
+    exit 1
+    ;;
+esac
+    
 make -C src/qhypstub CROSS_COMPILE=aarch64-linux-gnu-
 
 # patch to reduce mmc speed as some boards have intermittent failures when
 # inititalizing the mmc (maybe due to using old/recycled flash chips)
 echo 'DEFINES += USE_TARGET_HS200_CAPS=1' >> src/lk2nd/project/lk1st-msm8916.mk
 
-make -C src/lk2nd LK2ND_BUNDLE_DTB="msm8916-512mb-mtp.dtb" LK2ND_COMPATIBLE="yiming,uz801-v3" \
+make -C src/lk2nd LK2ND_BUNDLE_DTB="msm8916-512mb-mtp.dtb" LK2ND_COMPATIBLE="$DESCR" \
     TOOLCHAIN_PREFIX=arm-none-eabi- lk1st-msm8916 -j$(nproc)
 
 # test sign
